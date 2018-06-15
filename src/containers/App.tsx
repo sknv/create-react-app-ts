5825e1c1719b56c6690b8924ce2be273fa4df7f7
skanukov <s.kanukov@gmail.com>
2018-06-15 14:10:59 +0300

    Restructure the app and prettify code

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,22 +1,20 @@
-import * as React from 'react';
-import './App.css';
+import * as React from 'react'
 
-import logo from './logo.svg';
+import './App.css'
+import logo from './logo.svg'
 
-class App extends React.Component {
+export default class App extends React.Component {
   public render() {
     return (
       <div className="text-center">
         <header className="bg-blue-dark m-6 p-6 rounded">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="app-logo" alt="logo" />
           <h1 className="text-white">Welcome to React</h1>
         </header>
         <p>
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './css/tailwind.css';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+import App from './containers/App'
+import './tailwind.css'
+
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
