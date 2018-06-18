632ddcac7b8afec4a7ae8c737848a95a9b819be8
skanukov <s.kanukov@gmail.com>
2018-06-18 13:47:23 +0300

    Add badges

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,17 +7,20 @@ export default function Menu() {
       <div className="mb-6">
         <MenuHeader>MENU 1</MenuHeader>
         <ul>
+          <MenuLink exact={true} to="/admin">
+            Main
+            <MenuBadge>2</MenuBadge>
+          </MenuLink>
           <MenuLink to="/admin/item1">Menu Item 1</MenuLink>
-          <MenuLink to="/admin/item2">Menu Item 2</MenuLink>
         </ul>
       </div>
 
       <div className="mb-6">
         <MenuHeader>MENU 2</MenuHeader>
         <ul>
+          <MenuLink to="/admin/item2">Menu Item 2</MenuLink>
           <MenuLink to="/admin/item3">Menu Item 3</MenuLink>
           <MenuLink to="/admin/item4">Menu Item 4</MenuLink>
-          <MenuLink to="/admin/item5">Menu Item 5</MenuLink>
         </ul>
       </div>
     </nav>
@@ -37,16 +40,36 @@ function MenuHeader({ children }: IMenuHeaderProps) {
 }
 
 interface IMenuLinkProps {
+  exact?: boolean
   to: string
   children: React.ReactNode
 }
 
-function MenuLink({ to, children }: IMenuLinkProps) {
+function MenuLink({ exact, to, children }: IMenuLinkProps) {
   return (
-    <li className="mb-3">
-      <NavLink className="block text-grey-dark hover:text-black" to={to}>
+    <li className="mb-1">
+      <NavLink
+        className="block relative py-1 text-grey-dark hover:text-black"
+        exact={exact}
+        to={to}
+      >
         {children}
       </NavLink>
     </li>
+  )
+}
+
+interface IMenuBadgeProps {
+  children: React.ReactNode
+}
+
+function MenuBadge({ children }: IMenuBadgeProps) {
+  return (
+    <span
+      className="absolute pin-r px-2 rounded-full bg-red text-sm text-white"
+      style={{ top: '0.25rem', bottom: '0.25rem' }}
+    >
+      {children}
+    </span>
   )
 }
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,10 @@ import { NavLink } from 'react-router-dom'
 export default function Footerbar() {
   return (
     <ul className="lg:hidden fixed h-10 pin-b pin-x z-100 flex bg-grey-light footer">
-      <FooterLink to="/admin/item1">ITEM 1</FooterLink>
+      <FooterLink exact={true} to="/admin">
+        MAIN
+        <FooterBadge>2</FooterBadge>
+      </FooterLink>
       <FooterLink to="/admin/item2">ITEM 2</FooterLink>
       <FooterLink to="/admin/menu">MENU</FooterLink>
     </ul>
@@ -16,20 +19,36 @@ export default function Footerbar() {
 // ----------------------------------------------------------------------------
 
 interface IFooterLinkProps {
+  exact?: boolean
   to: string
   children: React.ReactNode
 }
 
-function FooterLink({ to, children }: IFooterLinkProps) {
+function FooterLink({ exact, to, children }: IFooterLinkProps) {
   return (
     <li className="flex-1">
       <NavLink
-        className="block px-4 text-sm text-grey-dark text-center"
-        style={{ lineHeight: '2.5rem' }}
+        className="block relative h-full py-3 px-4 text-sm text-grey-dark text-center"
+        exact={exact}
         to={to}
       >
         {children}
       </NavLink>
     </li>
+  )
+}
+
+interface IFooterBadgeProps {
+  children: React.ReactNode
+}
+
+function FooterBadge({ children }: IFooterBadgeProps) {
+  return (
+    <span
+      className="absolute px-2 rounded-full bg-red text-sm text-white"
+      style={{ top: '0.5rem', right: '0.5rem' }}
+    >
+      {children}
+    </span>
   )
 }
