7ae8f1c29c49e05ba59e0776da3304d30d261099
skanukov <s.kanukov@gmail.com>
2018-06-18 14:31:10 +0300

    Use an async profile action sample

--- src/pages/admin/Profile.tsx
+++ src/pages/admin/Profile.tsx
@@ -3,21 +3,44 @@ import { Link } from 'react-router-dom'
 
 import '../Index.css'
 
+import { fetchProfile, IProfile } from '../../actions/profile'
 import Page from '../../components/layout/Page'
 
-export default function Profle() {
-  const actions = (
-    <Link className="text-grey-dark hover:text-black" to="/logout">
-      Logout
-    </Link>
-  )
+interface IProfileState {
+  profile: IProfile
+}
 
-  return (
-    <Page header="My Profile" actions={actions} back={true}>
-      <section className="p-4 border border-grey-light rounded">
-        <h3 className="mb-3">My Profile</h3>
-        Welcome, "My Name"
-      </section>
-    </Page>
-  )
+export default class Profle extends React.PureComponent<any, IProfileState> {
+  public state = {
+    profile: {
+      name: ''
+    }
+  }
+
+  public async componentDidMount() {
+    try {
+      const profile = await fetchProfile()
+      this.setState({ profile })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  public render() {
+    const profile = this.state.profile
+    const actions = (
+      <Link className="text-grey-dark hover:text-black" to="/logout">
+        Logout
+      </Link>
+    )
+
+    return (
+      <Page header="My Profile" actions={actions} back={true}>
+        <section className="p-4 border border-grey-light rounded">
+          <h3 className="mb-3">My Profile</h3>
+          Welcome, {profile.name}
+        </section>
+      </Page>
+    )
+  }
 }
